3b0dbad6a4d6c3d488877335ce0f14d07faaed3c
Uzair Abdullah <uzairabdullah973@gmail.com>
2022-10-26 21:35:14 +0500

    added gradient

--- src/components/workWithMe.js
+++ src/components/workWithMe.js
@@ -9,10 +9,10 @@ const WorkWithMe = () => {
 				p: "30px",
 				mt: "10px",
 				mb: "10px",
-				backgroundImage: "url('/images/work_with_me.jpg')",
-				backgroundRepeat: "no-repeat",
-				backgroundSize: "cover",
-				minHeight: "70vh",
+				//background: 'rgb(11,12,24)',
+				background:
+					"linear-gradient(90deg, rgba(11,12,24,1) 14%, rgba(1,29,70,1) 53%, rgba(52,65,121,1) 81%)",
+				//minHeight: "60vh",
 			}}
 		>
 			<Stack
@@ -20,14 +20,26 @@ const WorkWithMe = () => {
 				justifyContent="center"
 				alignItems="center"
 				spacing={3}
+				sx={{ p: "10px" }}
 			>
 				<Typography component="h3" variant="h3">
-					Want to work with me?
+					Interested in working with me?
 				</Typography>
 				<Typography component="h3" variant="h4">
 					Contact me on Fiverr and we can start working together!
 				</Typography>
-				<Button variant="contained">Contact me!</Button>
+				<Button
+					variant="contained"
+					sx={{
+						fontSize: "18px",
+						background:
+							"linear-gradient(108.51deg, #F219A1 53.69%, #AD0CF8 100.22%, #FE007E 100.23%)",
+					}}
+					href="https://www.fiverr.com/uzairabdullah07"
+					target="_blank"
+				>
+					Contact me!
+				</Button>
 			</Stack>
 		</Container>
 	);
